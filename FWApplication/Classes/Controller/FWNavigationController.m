1c4aa65591119a30705403b9bdabb7c96ea01207
wuyong <lingshizhuangzi@gmail.com>
2021-11-22 13:20:45 +0800

    feat: 优化导航栏样式处理

--- FWApplication/Classes/Controller/FWNavigationController.m
+++ FWApplication/Classes/Controller/FWNavigationController.m
@@ -59,8 +59,13 @@
     
     if (@available(iOS 13.0, *)) {
         self.standardAppearance = navigationBar.standardAppearance;
-        self.compactAppearance = navigationBar.standardAppearance;
+        self.compactAppearance = navigationBar.compactAppearance;
         self.scrollEdgeAppearance = navigationBar.scrollEdgeAppearance;
+        #if __IPHONE_15_0
+        if (@available(iOS 15.0, *)) {
+            self.compactScrollEdgeAppearance = navigationBar.compactScrollEdgeAppearance;
+        }
+        #endif
     }
 }
 
