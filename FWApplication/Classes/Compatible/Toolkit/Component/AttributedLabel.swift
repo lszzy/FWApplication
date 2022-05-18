b696531e4f39e82021ce1caeaadf3d12a04d6077
wuyong <lingshizhuangzi@gmail.com>
2022-05-18 17:40:32 +0800

    feat: Swift重构Toolkit

---
+++ FWApplication/Classes/Compatible/Toolkit/Component/AttributedLabel.swift
@@ -0,0 +1,38 @@
+//
+//  AttributedLabel.swift
+//  FWApplication
+//
+//  Created by wuyong on 2022/5/18.
+//
+
+import UIKit
+
+extension Wrapper where Base: NSMutableAttributedString {
+    
+    public var textColor: UIColor? {
+        get { return base.__fw.textColor }
+        set { base.__fw.textColor = newValue }
+    }
+    
+    public func setTextColor(_ color: UIColor, range: NSRange) {
+        base.__fw.setTextColor(color, range: range)
+    }
+
+    public var font: UIFont? {
+        get { return base.__fw.font }
+        set { base.__fw.font = newValue }
+    }
+    
+    public func setFont(_ font: UIFont, range: NSRange) {
+        base.__fw.setFont(font, range: range)
+    }
+
+    public func setUnderlineStyle(_ style: CTUnderlineStyle, modifier: CTUnderlineStyleModifiers) {
+        base.__fw.setUnderlineStyle(style, modifier: modifier)
+    }
+    
+    public func setUnderlineStyle(_ style: CTUnderlineStyle, modifier: CTUnderlineStyleModifiers, range: NSRange) {
+        base.__fw.setUnderlineStyle(style, modifier: modifier, range: range)
+    }
+    
+}
