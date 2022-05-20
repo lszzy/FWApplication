2c686e042cd69a6dfce6b82810d5519e003148be
wuyong <lingshizhuangzi@gmail.com>
2022-05-20 22:42:44 +0800

    feat: Swift重构Toolkit

---
+++ FWApplication/Classes/Compatible/Toolkit/UIKit/UIDevice+Toolkit.swift
@@ -0,0 +1,49 @@
+//
+//  UIDevice+Toolkit.swift
+//  FWApplication
+//
+//  Created by wuyong on 2022/5/20.
+//
+
+import UIKit
+
+extension Wrapper where Base: UIDevice {
+    
+    // MARK: - UUID
+
+    /// 获取或设置设备UUID，自动keychain持久化。默认获取IDFV(未使用IDFA，避免额外权限)，失败则随机生成一个
+    public static var deviceUUID: String {
+        get { return Base.__fw.deviceUUID }
+        set { Base.__fw.deviceUUID = newValue }
+    }
+
+    // MARK: - Jailbroken
+
+    // 是否越狱
+    public static var isJailbroken: Bool {
+        return Base.__fw.isJailbroken
+    }
+
+    // MARK: - Network
+
+    // 本地IP地址
+    public static var ipAddress: String? {
+        return Base.__fw.ipAddress
+    }
+    
+    // 本地主机名称
+    public static var hostName: String? {
+        return Base.__fw.hostName
+    }
+    
+    // 手机运营商名称
+    public static var carrierName: String? {
+        return Base.__fw.carrierName
+    }
+    
+    // 手机蜂窝网络类型，仅区分2G|3G|4G|5G
+    public static var networkType: String? {
+        return Base.__fw.networkType
+    }
+    
+}
---
+++ FWApplication/Classes/Compatible/Toolkit/UIKit/UIFont+Toolkit.swift
@@ -0,0 +1,66 @@
+//
+//  UIFont+Toolkit.swift
+//  FWApplication
+//
+//  Created by wuyong on 2022/5/20.
+//
+
+import UIKit
+
+extension Wrapper where Base: UIFont {
+    
+    // MARK: - Font
+
+    /// 是否是粗体
+    public var isBold: Bool {
+        return base.__fw.isBold
+    }
+
+    /// 是否是斜体
+    public var isItalic: Bool {
+        return base.__fw.isItalic
+    }
+
+    /// 当前字体的粗体字体
+    public var boldFont: UIFont {
+        return base.__fw.boldFont
+    }
+    
+    /// 当前字体的非粗体字体
+    public var nonBoldFont: UIFont {
+        return base.__fw.nonBoldFont
+    }
+    
+    /// 当前字体的斜体字体
+    public var italicFont: UIFont {
+        return base.__fw.italicFont
+    }
+    
+    /// 当前字体的非斜体字体
+    public var nonItalicFont: UIFont {
+        return base.__fw.nonItalicFont
+    }
+    
+    // MARK: - Height
+
+    // 字体空白高度(上下之和)
+    public var spaceHeight: CGFloat {
+        return base.__fw.spaceHeight
+    }
+
+    // 根据字体计算指定倍数行间距的实际行距值(减去空白高度)，示例：行间距为0.5倍实际高度
+    public func lineSpacing(multiplier: CGFloat) -> CGFloat {
+        return base.__fw.lineSpacing(withMultiplier: multiplier)
+    }
+
+    // 根据字体计算指定倍数行高的实际行高值(减去空白高度)，示例：行高为1.5倍实际高度
+    public func lineHeight(multiplier: CGFloat) -> CGFloat {
+        return base.__fw.lineHeight(withMultiplier: multiplier)
+    }
+
+    /// 计算当前字体与指定字体居中对齐的偏移值
+    public func baselineOffset(_ font: UIFont) -> CGFloat {
+        return base.__fw.baselineOffset(font)
+    }
+    
+}
