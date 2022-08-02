158fb602b7435bf9b5d8b4762944ef1e6b16484a
wuyong <lingshizhuangzi@gmail.com>
2022-08-02 22:09:30 +0800

    feat: SwiftUI目录结构调整

---
+++ FWApplication/Classes/SwiftUI/Module/Model/ViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ViewModel.swift
+//  FWApplication
+//
+//  Created by wuyong on 2022/7/28.
+//
+
+#if canImport(SwiftUI)
+import SwiftUI
+
+/// ViewModel基类
+@available(iOS 13.0, *)
+open class ViewModel: ObservableObject {}
+
+#endif
